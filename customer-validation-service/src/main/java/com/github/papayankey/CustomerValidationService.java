f949f9f21b860818ad801ba4d9d2c9fff8ecd19a
Benneth Yankey <yankeybenneth@gmail.com>
2023-06-15 10:21:49 +0000

    initial commit

---
+++ customer-validation-service/src/main/java/com/github/papayankey/CustomerValidationService.java
@@ -0,0 +1,11 @@
+package com.github.papayankey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerValidationService {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerValidationService.class, args);
+    }
+}
