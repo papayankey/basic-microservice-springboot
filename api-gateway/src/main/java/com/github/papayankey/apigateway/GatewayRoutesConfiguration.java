6939f0f97dcf1da9fac69cf51435de6d44b72284
Benneth Yankey <yankeybenneth@gmail.com>
2023-09-06 09:43:17 +0000

    add more routes to gateway programmatic configuration

--- api-gateway/src/main/java/com/github/papayankey/apigateway/GatewayRoutesConfiguration.java
+++ api-gateway/src/main/java/com/github/papayankey/apigateway/GatewayRoutesConfiguration.java
@@ -10,10 +10,32 @@ public class GatewayRoutesConfiguration {
     @Bean
     RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("route-one", p -> p.path("/verify/{id}")
+                // verify customer
+                .route("verify-customer", p -> p.path("/verify/{id}")
                         .filters(f -> f.setPath("/customers/verify/{id}"))
                         .uri("lb://customer-service"))
-                .route("route-two", p -> p.path("/customers/verify/{id}")
+                .route("verify-customer", p -> p.path("/customers/verify/{id}")
+                        .uri("lb://customer-service"))
+
+                // get a customer
+                .route("get-customer", p -> p.path("/info/{id}")
+                        .filters(f -> f.setPath("/customers/{id}"))
+                        .uri("lb://customer-service"))
+                .route("get-customer", p -> p.path("/customers/{id}")
+                        .uri("lb://customer-service"))
+
+                // get customers
+                .route("get-customers", p -> p.path("/info")
+                        .filters(f -> f.setPath("/customers"))
+                        .uri("lb://customer-service"))
+                .route("get-customers", p -> p.path("/customers")
+                        .uri("lb://customer-service"))
+
+                // add customer
+                .route("add-customer", p -> p.path("/info").and().method("POST")
+                        .filters(f -> f.setPath("/customers"))
+                        .uri("lb://customer-service"))
+                .route("add-customer", p -> p.path("/customers").and().method("POST")
                         .uri("lb://customer-service"))
                 .build();
     }
