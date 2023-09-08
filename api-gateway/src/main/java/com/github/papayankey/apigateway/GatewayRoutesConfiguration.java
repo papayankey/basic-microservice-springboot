83514b90e2b8aa78bf8fd11626016226f3f145be
Benneth Yankey <yankeybenneth@gmail.com>
2023-09-08 09:40:01 +0000

    change setPath to rewritePath

--- api-gateway/src/main/java/com/github/papayankey/apigateway/GatewayRoutesConfiguration.java
+++ api-gateway/src/main/java/com/github/papayankey/apigateway/GatewayRoutesConfiguration.java
@@ -10,33 +10,42 @@ public class GatewayRoutesConfiguration {
     @Bean
     RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
+                .route("verify-customer", p -> p.path("/verify/**")
+                        .filters(f -> f.rewritePath("/verify/(?<name>.*)", "/customers/verify/${name}"))
+                        .uri("lb://customer-service"))
+                .route("verify-customer", p -> p.path("/customers/**")
+                        .uri("lb://customer-service"))
+                .route("get-customer", p -> p.path("/info/**")
+                        .filters(f -> f.rewritePath("/info/(?<segment>.*)", "/customers/info/${segment}"))
+                        .uri("lb://customer-service"))
+
                 // verify customer
-                .route("verify-customer", p -> p.path("/verify/{id}")
-                        .filters(f -> f.setPath("/customers/verify/{id}"))
-                        .uri("lb://customer-service"))
-                .route("verify-customer", p -> p.path("/customers/verify/{id}")
-                        .uri("lb://customer-service"))
+//                .route("verify-customer", p -> p.path("/verify/{id}")
+//                        .filters(f -> f.setPath("/customers/verify/{id}"))
+//                        .uri("lb://customer-service"))
+//                .route("verify-customer", p -> p.path("/customers/verify/{id}")
+//                        .uri("lb://customer-service"))
 
                 // get a customer
-                .route("get-customer", p -> p.path("/info/{id}")
-                        .filters(f -> f.setPath("/customers/{id}"))
-                        .uri("lb://customer-service"))
-                .route("get-customer", p -> p.path("/customers/{id}")
-                        .uri("lb://customer-service"))
+//                .route("get-customer", p -> p.path("/info/{id}")
+//                        .filters(f -> f.setPath("/customers/{id}"))
+//                        .uri("lb://customer-service"))
+//                .route("get-customer", p -> p.path("/customers/{id}")
+//                        .uri("lb://customer-service"))
 
                 // get customers
-                .route("get-customers", p -> p.path("/info")
-                        .filters(f -> f.setPath("/customers"))
-                        .uri("lb://customer-service"))
-                .route("get-customers", p -> p.path("/customers")
-                        .uri("lb://customer-service"))
+//                .route("get-customers", p -> p.path("/info")
+//                        .filters(f -> f.setPath("/customers"))
+//                        .uri("lb://customer-service"))
+//                .route("get-customers", p -> p.path("/customers")
+//                        .uri("lb://customer-service"))
 
                 // add customer
-                .route("add-customer", p -> p.path("/info").and().method("POST")
-                        .filters(f -> f.setPath("/customers"))
-                        .uri("lb://customer-service"))
-                .route("add-customer", p -> p.path("/customers").and().method("POST")
-                        .uri("lb://customer-service"))
+//                .route("add-customer", p -> p.path("/info").and().method("POST")
+//                        .filters(f -> f.setPath("/customers"))
+//                        .uri("lb://customer-service"))
+//                .route("add-customer", p -> p.path("/customers").and().method("POST")
+//                        .uri("lb://customer-service"))
                 .build();
     }
 }
